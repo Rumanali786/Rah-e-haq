16ca64ce242bfe81bf1daf55b7f51a403e8d6dcb
DESKTOP-L1J6937\Ruman ali <jutts1055@gmail.com>
2021-02-08 15:51:29 +0500

    comit

---
+++ android/app/src/main/kotlin/com/rah_e_haq/rah_e_haq/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rah_e_haq.rah_e_haq
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:rah_e_haq/screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
